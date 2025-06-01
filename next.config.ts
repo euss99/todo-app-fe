9310d7aa86c989daff2ba553a7af1bebc5dd64d9
Uriel Solis Salinas <euss99@hotmail.com>
2025-06-01 14:38:18 -0600

    chore: update Next.js configuration and build script
    - Set output to 'export' in next.config.ts for static site generation.
    - Remove next export from build script in package.json to streamline the build process.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const config: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
   },
+  output: 'export',
   images: {
     unoptimized: true,
   },
