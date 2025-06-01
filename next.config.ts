e37753174c31a6fff2399cfaf2b97c5f39463b9f
Uriel Solis Salinas <euss99@hotmail.com>
2025-06-01 14:34:46 -0600

    chore: update Next.js configuration for improved environment handling
    - Add unoptimized images setting to next.config.ts.
    - Modify basePath to dynamically set based on the production environment.
    - Introduce environment variable for API URL with a fallback for local development.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import { type NextConfig } from "next";
 
 const config: NextConfig = {
   output: 'export',
-  trailingSlash: true,
-  basePath: '/todo-app-fe',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/todo-app-fe' : '',
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+  },
 };
 
 export default config;
