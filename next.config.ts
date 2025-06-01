b12c4b802786290d6d4f3ab25304ce1537fafc27
Uriel Solis Salinas <euss99@hotmail.com>
2025-06-01 14:36:39 -0600

    chore: simplify Next.js configuration by removing basePath setting
    - Remove basePath configuration from next.config.ts.
    - Retain unoptimized images setting and environment variable for API URL.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import { type NextConfig } from "next";
 
 const config: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/todo-app-fe' : '',
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
