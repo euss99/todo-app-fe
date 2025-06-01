4f958fe38b718bfb8272fda3a83cc4d052d77240
Uriel Solis Salinas <euss99@hotmail.com>
2025-06-01 15:30:33 -0600

    chore: update Next.js configuration to standardize output format and remove trailingSlash setting

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,10 @@ const config: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
   },
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 export default config;
